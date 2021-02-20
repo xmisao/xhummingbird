aae21816392ea3995006d0e199522b5acef3fc4a
xmisao <mail@xmisao.com>
2021-02-20 18:06:49 +0900

    Receive protobuf request using zmq subscriber

--- xhummingbird_server/src/main.rs
+++ xhummingbird_server/src/main.rs
@@ -1,3 +1,19 @@
+use xhummingbird_server::protos::request::Request;
+use protobuf::Message;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "tcp://*:8800";
+    let context = zmq::Context::new();
+    let subscriber = context.socket(zmq::SUB).unwrap();
+
+    assert!(subscriber.bind(address).is_ok());
+    assert!(subscriber.set_subscribe("".as_bytes()).is_ok()); // NOTE: Subscribe all
+
+    println!("xHummingbird server started at {}", address);
+
+    loop {
+        let bytes = subscriber.recv_bytes(0).unwrap();
+        let request = Request::parse_from_bytes(&bytes).unwrap();
+        println!("{:?}", request);
+    }
 }
