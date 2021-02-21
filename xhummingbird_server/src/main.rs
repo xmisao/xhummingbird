04425d6b3ea7884f7415d800907460bb75c0b952
xmisao <mail@xmisao.com>
2021-02-21 18:49:37 +0900

    Introduce control thread

--- xhummingbird_server/src/main.rs
+++ xhummingbird_server/src/main.rs
@@ -1,27 +1,68 @@
 use xhummingbird_server::protos::request::Request;
 use protobuf::Message;
+use std::sync::{Mutex, Arc};
 use std::thread;
-use std::sync::mpsc;
+use std::thread::Thread;
+use std::thread::JoinHandle;
+use std::io;
 
 fn main() {
-    let address = "tcp://*:8800";
-    let context = zmq::Context::new();
-    let subscriber = context.socket(zmq::SUB).unwrap();
-    let mut store = Store::new();
+    let store = Arc::new(Mutex::new(Store::new()));
+    let worker_reference = Arc::clone(&store);
+    let control_reference = Arc::clone(&store);
 
-    assert!(subscriber.bind(address).is_ok());
-    assert!(subscriber.set_subscribe("".as_bytes()).is_ok()); // NOTE: Subscribe all
+    let worker_thread = start_worker_thread(worker_reference);
+    let control_thread = start_control_thread(control_reference);
 
-    println!("xHummingbird server started at {}", address);
+    worker_thread.join().unwrap();
+    control_thread.join().unwrap();
+}
 
-    loop {
-        let bytes = subscriber.recv_bytes(0).unwrap();
-        let request = Request::parse_from_bytes(&bytes).unwrap();
+fn start_worker_thread(store_reference: Arc<Mutex<Store>>) -> JoinHandle<Thread> {
+    thread::spawn(move || {
+        let address = "tcp://*:8800";
+        let context = zmq::Context::new();
+        let subscriber = context.socket(zmq::SUB).unwrap();
+        assert!(subscriber.bind(address).is_ok());
+        assert!(subscriber.set_subscribe("".as_bytes()).is_ok()); // NOTE: Subscribe all
 
-        // println!("{:?}", request);
-        store.put(request);
-        store.print();
-    }
+        println!("xHummingbird server started at {}", address);
+
+        loop {
+            let bytes = subscriber.recv_bytes(0).unwrap();
+            let request = Request::parse_from_bytes(&bytes).unwrap();
+
+            let mut store = store_reference.lock().unwrap();
+            store.put(request);
+        }
+    })
+}
+
+fn start_control_thread(store_reference: Arc<Mutex<Store>>) -> JoinHandle<Thread> {
+    thread::spawn(move || {
+        loop {
+            let mut input = String::new();
+
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    let input = input.trim();
+
+                    match &*input {
+                        "print" => {
+                            println!("Requests:");
+                            let store = store_reference.lock().unwrap();
+                            store.print();
+                        },
+                        _ => {
+                            println!("Unknown command: {}", input);
+                        }
+
+                    }
+                },
+                Err(error) => println!("Error: {}", error),
+            }
+        };
+    })
 }
 
 struct Store {
