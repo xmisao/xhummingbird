86004fa0de0792b488ca0bf195ce567eb6e65774
xmisao <mail@xmisao.com>
2021-03-27 13:44:11 +0900

    Prevent unexpected worker aboart

--- xhummingbird_server/src/workers/receiver_worker.rs
+++ xhummingbird_server/src/workers/receiver_worker.rs
@@ -5,25 +5,34 @@ use crate::protos::event::Event;
 use std::thread;
 use actix::prelude::*;
 use protobuf::Message;
+use std::time::Duration;
 
 pub fn start(storage_actor_address: Addr<StorageActor>, notification_actor_address: Addr<NotificationActor>){
     thread::spawn(move || {
-        let address = "tcp://*:8800";
-        let context = zmq::Context::new();
-        let subscriber = context.socket(zmq::PULL).unwrap();
-        assert!(subscriber.bind(address).is_ok());
-
-        println!("xHummingbird event receiver started at {}", address);
-
         loop {
-            let bytes = subscriber.recv_bytes(0).unwrap();
-            let event = Event::parse_from_bytes(&bytes).unwrap();
-            println!("{:?}", event);
-
-            let storage_actor_address = storage_actor_address.clone();
-
-            println!("{:?}", storage_actor_address.try_send(PutEvent{event: event.clone()}).unwrap());
-            println!("{:?}", notification_actor_address.try_send(PutEvent{event: event.clone()}).unwrap());
+            run(storage_actor_address.clone(), notification_actor_address.clone());
+            println!("Unexpected run() aborted.");
+            thread::sleep(Duration::from_millis(1000));
         }
     });
+}
+
+pub fn run(storage_actor_address: Addr<StorageActor>, notification_actor_address: Addr<NotificationActor>) -> Option<u8>{
+    let address = "tcp://*:8800";
+    let context = zmq::Context::new();
+    let subscriber = context.socket(zmq::PULL).ok()?;
+    assert!(subscriber.bind(address).is_ok());
+
+    println!("xHummingbird event receiver started at {}", address);
+
+    loop {
+        let bytes = subscriber.recv_bytes(0).ok()?;
+        let event = Event::parse_from_bytes(&bytes).ok()?;
+        println!("{:?}", event);
+
+        let storage_actor_address = storage_actor_address.clone();
+
+        println!("{:?}", storage_actor_address.try_send(PutEvent{event: event.clone()}).ok()?);
+        println!("{:?}", notification_actor_address.try_send(PutEvent{event: event.clone()}).ok()?);
+    }
 }
