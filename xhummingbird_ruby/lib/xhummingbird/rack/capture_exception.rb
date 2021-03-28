45b2d1dc634b8154fd1855303d476027ea419fbd
xmisao <mail@xmisao.com>
2021-03-28 13:11:57 +0900

    Fix conversion of rack envorinment

--- xhummingbird_ruby/lib/xhummingbird/rack/capture_exception.rb
+++ xhummingbird_ruby/lib/xhummingbird/rack/capture_exception.rb
@@ -1,13 +1,28 @@
 module Xhummingbird
   module Rack
     class CaptureException
+      CONVERT_KEYS = Set.new(%w(
+        REQUEST_METHOD
+        SCRIPT_NAME
+        PATH_INFO
+        QUERY_STRING
+        SERVER_NAME
+        SERVER_PORT
+        rack.version
+        rack.url_scheme
+        rack.multithread
+        rack.multiprocess
+        rack.run_once
+        rack.hijack?
+      ))
+
+      HTTP_HEADER_PREFIX = 'HTTP_'
+
       def initialize(app)
         @app = app
       end
 
       def call(env)
-        p env
-
         begin
           response = @app.call(env)
         rescue => e
@@ -27,7 +42,9 @@ module Xhummingbird
         tags = {}
 
         env.each do |k, v|
-          tags["rack_env/" +  k.to_s] = v.to_s rescue nil
+          if CONVERT_KEYS.include?(k) || k.start_with?(HTTP_HEADER_PREFIX)
+            tags["rack_env/" +  k.to_s] = v.to_s rescue nil
+          end
         end
 
         tags
