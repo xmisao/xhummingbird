dcc80e6d369b358a044ec5b35f53e57d9e6d392e
xmisao <mail@xmisao.com>
2021-05-09 15:43:30 +0900

    Link to service filter

--- xhummingbird_server/src/tmpl_helper.rs
+++ xhummingbird_server/src/tmpl_helper.rs
@@ -1,6 +1,12 @@
-pub fn title_to_title_link(title: &String) -> String {
+pub fn filter_link(title: Option<String>, service: Option<String>) -> String {
     let mut encoded = form_urlencoded::Serializer::new(String::new());
-    encoded.append_pair("title", title);
+
+    if title.is_some() {
+        encoded.append_pair("title", &title.unwrap());
+    }
+    if service.is_some() {
+        encoded.append_pair("service", &service.unwrap());
+    }
 
     format!("/events?{}", encoded.finish())
 }
